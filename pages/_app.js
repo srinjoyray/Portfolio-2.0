61427b8ed91192451dd6472d708967bf1370552f
Srinjoy Ray <srinjoyray@yahoo.in>
2022-01-10 10:32:22 +0530

    Fixed bugs

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,18 @@ import { useEffect } from 'react';
 import { keepTheme } from '../utils/themes';
 import '../styles/globals.css'
 
+
+
+
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     keepTheme();
   }, [])
   
-  return <Component {...pageProps} />
+  return(   
+      <Component {...pageProps} />
+    ) 
+    
 }
 
 export default MyApp
